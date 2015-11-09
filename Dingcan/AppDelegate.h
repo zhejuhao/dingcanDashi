1acef18f9ea6009c194281eb35f4f8d54508ffba
李旗 <liqi@riguangsongtekiMacBook-Air.local>
2015-11-09 10:05:23 +0800

    Initial Commit

---
+++ Dingcan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dingcan
+//
+//  Created by 李旗 on 15/11/9.
+//  Copyright (c) 2015年 李旗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dingcan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dingcan
+//
+//  Created by 李旗 on 15/11/9.
+//  Copyright (c) 2015年 李旗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
